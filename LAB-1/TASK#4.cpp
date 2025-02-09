29ca0b2a7bfaa2bae624b663e6a5794622987cbd
Jiyanshu Thourani <janshuthourani123@gmail.com>
2025-02-09 10:29:16 -0800

    Create TASK#4.cpp

---
+++ LAB-1/TASK#4.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void inputuser(string name[],int row){
+    for (int i = 0; i <row; i++)
+    {
+      cout<<"Name["<<i+1<<"]: ";
+      getline(cin,name[i]);
+    }
+    
+}
+void display(string name[],int row){
+    for (int i = 0; i <row; i++)
+    {
+       cout<<"Name["<<i+1<<"] = "<<name[i]<<endl;
+    }
+    
+}
+bool attendence_checker(string name1[],string name2[],int row,string searchName){
+    for (int  i = 0; i < row; i++)
+    {
+      if(name1[i]==searchName || name2[i]==searchName){
+       return true;
+      }  
+    }
+    return false;
+}
+int total_donnation(int max_input1,int max_input2){
+    const int max_donation = 10;
+   return max_donation*(max_input1+max_input2);
+}
+void reverse(string name[],int row){
+    for (int  i = row-1; i>0; i--)
+    {
+        cout<<"Name["<<i+1<<"] = "<<name[i]<<endl;
+    }
+    
+}
+int main()
+{
+const int row = 5;
+string name1[row];
+string name2[row];
+cout<<"Enter Name of participants for event 1:"<<endl;
+inputuser(name1,row);
+cout<<"Enter Name of participants for event 2:"<<endl;
+inputuser(name2,row);
+cout<<"Displaying the names of participants for event 1"<<endl;
+display(name1,row);
+cout<<"__________________________________________"<<endl;
+cout<<"Displaying the names of participants for event 2"<<endl;
+display(name2,row);
+string searchname;
+cout<<"Enter Name you want to search: "<<endl;
+getline(cin,searchname);
+bool ac=attendence_checker(name1,name2,row,searchname);
+if (ac==true)
+{
+    cout<<"Name is Registered"<<endl;
+}
+else
+{
+    cout<<"NON-REGISTERED"<<endl;
+}
+cout<<"total donation = "<<total_donnation(row,row)<<endl;
+cout<<"reversing the name of participant for event 1"<<endl;
+reverse(name1,row);
+cout<<"_____________________________________________________"<<endl;
+cout<<"reversing the name of participant for event 2"<<endl;
+reverse(name2,row);
+return 0;
+}
